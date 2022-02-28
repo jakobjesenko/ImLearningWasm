d7e90c41ccc98cd83c6c2b5d3a7f51b51358dc5a
jakobjesenko <jj6041@student.uni-lj.si>
2022-02-28 11:51:00 +0100

    fixed token position, tokens readout

--- badscript/compiler.c
+++ badscript/compiler.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <stdbool.h>
 
+#define PROGRAM_LENGTH 1024
+
 typedef enum{
     op_exit
 } ops;
@@ -26,13 +28,14 @@ bool isWhiteSpace(char c){
     return c == ' ' || c == '\t' || c == '\n' ? true : false;
 }
 
-// TODO token position not working corectly
 void lex(char* codeFile, token program[]){
     FILE* f = fopen(codeFile, "r");
     char c;
+    char prevC;
     char word[32];
-    int col = 0;
-    int row = 0;
+    int col = 1;
+    int prevCol = 1;
+    int row = 1;
     int wordIndex = 0;
     int programCounter = 0;
     bool wordIsNumber = true;
@@ -56,26 +59,30 @@ void lex(char* codeFile, token program[]){
         }
         col++;
         if (isWhiteSpace(c) && wordIndex){
+            int line = prevC == '\n' ? row - 1 : row;
             if (wordIsNumber){
-                program[programCounter++] = (token){row, col - wordIndex, keywordCount, strtol(word, NULL, 10)};
+                program[programCounter++] = (token){line, prevCol - wordIndex, keywordCount - 1, strtol(word, NULL, 10)};
             } else {
                 for (int i = 0; i < keywordCount - nonKwordsCount; i++){
                     if (!strcmp(word, keywords[i])){
-                        program[programCounter++] = (token){row, col - wordIndex, i, 0};
+                        program[programCounter++] = (token){line, prevCol - wordIndex, i, 0};
                     }
                 }
             }
             wordIndex = 0;
             wordIsNumber = true;
         }
+        prevC = c;
+        prevCol = col;
     }
     if (wordIndex){
+        int line = prevC == '\n' ? row - 1 : row;
         if (wordIsNumber){
-            program[programCounter++] = (token){row, col - wordIndex, keywordCount, strtol(word, NULL, 10)};
+            program[programCounter++] = (token){line, prevCol - wordIndex, keywordCount - 1, strtol(word, NULL, 10)};
         } else {
             for (int i = 0; i < keywordCount - nonKwordsCount; i++){
                 if (!strcmp(word, keywords[i])){
-                    program[programCounter++] = (token){row, col - wordIndex, i, 0};
+                    program[programCounter++] = (token){line, prevCol - wordIndex, i, 0};
                 }
             }
         }
@@ -83,17 +90,29 @@ void lex(char* codeFile, token program[]){
     }
     fclose(f);
 }
-// TODO END
+
+void tokensToString(token* program){
+    token t;
+    for (int i = 0; i < PROGRAM_LENGTH; i++){
+        t = program[i];
+        if (!t.row){
+            return;
+        }
+        fprintf(stdout, "ln%d:col%d\t(%d)%s\t%d\n", t.row, t.col, t.opnum, keywords[t.opnum], t.value);
+    }
+}
 
 int main(int argc, char* argv[]){
     if (argc <= 1){
         printf("Not enough arguments!\n");
         return -1;
     }
-    static token program[1024];
+    static token program[PROGRAM_LENGTH];
     
     char* codeFile = argv[1];
     lex(codeFile, program);
+
+    tokensToString(program);
     
     return 0;
 }
